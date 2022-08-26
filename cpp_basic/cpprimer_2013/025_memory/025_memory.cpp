00a3eec3bfabde21c1e3e6901925312421d8f7a6
menglimeng <dream_15320@163.com>
2022-08-26 18:27:28 +0800

    update memery

---
+++ cpp_basic/cpprimer_2013/025_memory/025_memory.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <new>
+
+#include "StrBlob.h"
+
+using namespace std;
+
+void excise121()
+{
+    StrBlob b1;
+    {
+    StrBlob b2 = {"a", "an", "the"};
+    b1 = b2;
+    b2.push_back("about");
+    }
+
+    cout << b1.size() << endl;  // 4
+    // cout << b2.size() << endl;  // undefined
+}
+
+vector<int> * exercise126()
+{
+    return new vector<int>();
+}
+
+
+vector<int> * exercise126_1()
+{
+    vector<int> *ivec = exercise126();
+    int i;
+    while (cin >> i)
+    {
+        (*ivec).push_back(i);
+    }
+    return ivec;
+}
+
+void exercise126_2()
+{
+    vector<int> *ivec = exercise126_1();
+    for (const auto &i: *ivec)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    delete ivec;
+}
+
+shared_ptr<vector<int>> exercise127()
+{
+    return make_shared<vector<int>>();
+}
+
+shared_ptr<vector<int>> exercise127_1()
+{
+    auto ivec = exercise127();
+    int i;
+    while (cin >> i)
+    {
+        (*ivec).push_back(i);
+    }
+    return ivec;
+}
+
+void exercise127_2()
+{
+    auto ivec = exercise127_1();
+    for (const auto &i: *ivec)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void exercise1220()
+{
+    StrBlob sb;
+    ifstream in_f("./exer1220.txt");
+    string s;
+    while(getline(in_f, s))
+        sb.push_back(s);
+    
+    auto sz = sb.size();
+    StrBlobPtr beg(sb.begin());
+    for(decltype(sz) i = 0; i < sz; ++i)
+    {
+        cout << beg.deref() << endl;
+        beg.incr();
+    }
+}
+
+void exercise1223()
+{
+    string s1("aaa");
+    string s2("bbb");
+    auto sz = s1.size() + s2.size();
+    char *pc = new char[sz];
+    for (decltype(sz) i = 0; i < sz; ++i)
+        if (i < s1.size())
+            *(pc + i) = s1[i];
+        else
+            *(pc + i) = s2[i - s1.size()];
+    
+    for (decltype(sz) i = 0; i < sz; ++i)
+        cout << *(pc + i);
+    cout << endl;
+
+    delete [] pc;
+
+    cout << s1 + s2 << endl;
+}
+
+void allocator()
+{
+    int n = 20;
+    allocator<string> alloc; // object that can allocate strings
+    auto const p = alloc.allocate(n); // allocate n unconstructed strings
+
+    auto q = p; // q will point to one past the last constructed element
+    alloc.construct(q++); // *q is the empty string
+    alloc.construct(q++, 10, 'c'); // *q is cccccccccc
+    alloc.construct(q++, "hi"); // *q is hi!
+
+    cout << *(p+2) << endl;  // p+1 cccccccccc  p+2 hi
+    cout << p << endl << q << endl;
+
+    while (q != p)
+        alloc.destroy(--q); // free the strings we actually allocated
+
+    alloc.deallocate(p, n);
+
+    // construct elements starting at p as copies of elements in vi
+    vector<string> svec{"aa", "bb", "cc"};
+    auto p1 = uninitialized_copy(svec.begin(), svec.end(), p);
+    // initialize the remaining elements
+    uninitialized_fill_n(p1, svec.size(), "dd");
+}
+
+int main(int argc, char const *argv[])
+{
+    // exercise126_2();
+    // exercise127_2();
+    // exercise1220();
+    // exercise1223();
+
+    
+
+
+    return 0;
+}
+
---
+++ cpp_basic/cpprimer_2013/025_memory/StrBlob.h
@@ -0,0 +1,55 @@
+#ifndef STRBLOB_H
+#define STRBLOB_H
+
+#include <vector>
+#include <memory>
+
+class StrBlobPtr;
+
+class StrBlob {
+public:
+    friend class StrBlobPtr;
+    typedef std::vector<std::string>::size_type size_type;
+    StrBlob();
+    StrBlob(std::initializer_list<std::string> il);
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    // add and remove elements
+    void push_back(const std::string &t) {data->push_back(t);}
+    void pop_back();
+    // element access
+    std::string& front();
+    std::string& back();
+    const std::string& front() const;
+    const std::string& back() const;
+    StrBlobPtr begin();
+    StrBlobPtr end();
+    ~StrBlob(){}
+
+private:
+    std::shared_ptr<std::vector<std::string>> data;
+    // throws msg if data[i] isn't valid
+    void check(size_type i, const std::string &msg) const;
+};
+
+
+class StrBlobPtr
+{
+private:
+    // check returns a shared_ptr to the vector if the check succeeds
+    std::shared_ptr<std::vector<std::string>>
+        check(std::size_t, const std::string&) const;
+    // store a weak_ptr, which means the underlying vector might be destroyed
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr; // current position within the array
+public:
+    StrBlobPtr(): curr(0){}
+    StrBlobPtr(StrBlob &a, std::size_t sz = 0):wptr(a.data), curr(sz){}
+    std::string& deref() const;
+    StrBlobPtr& incr(); // prefix version
+    ~StrBlobPtr(){}
+};
+
+
+
+#endif
